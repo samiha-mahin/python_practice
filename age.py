7475280da8e383044f1df6fb9e8fe29616b6e9ae
samiha-mahin <samihamahin36@gmail.com>
2025-04-18 19:54:46 +0600

    python

--- age.py
+++ age.py
@@ -4,4 +4,4 @@ age = int (input("Enter Your Age: "))
 current_year = 2025
 year_100 = current_year + (100-age)
 
-print("Hello {name}! You will turn 100 years at the year of {year_100}")S
+print(f"Hello {name}! You will turn 100 years at the year of {year_100}")
